991f0d384971f46b67ffa2338672430fa4d43215
sirius <siriusblack7c@gmail.com>
2025-07-23 09:46:37 +0900

    cors fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,8 +36,7 @@ app.use(cors(
     {
         origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
+        allowedHeaders: ["Content-Type", "Authorization"]
     }
 ));
 
